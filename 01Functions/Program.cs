298ddfe36898533a90fd15ddb5f227c76d823767
39766MAweb <39766@ma-web.nl>
2025-11-18 12:22:58 +0100

    update

--- 01Functions/Program.cs
+++ 01Functions/Program.cs
@@ -1,9 +1,17 @@
-﻿namespace _01Functions;
-
-class Program
+﻿namespace _01Functions
 {
+    internal class Program
+    {
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Program program = new Program();
+        program Run();
+        // We hebben een variable genaamd "Program" gemaakt, als we Run uitvoeren dan voeren we "Program" uit
     }
+    
+
+
+    }
+
 }
