d126b18c8bc352d05022490233ad7643d5c8b471
39766MAweb <39766@ma-web.nl>
2025-11-18 13:42:25 +0100

    Work in Progress

--- 01Functions/Program.cs
+++ 01Functions/Program.cs
@@ -1,17 +1,45 @@
 ﻿namespace _01Functions
 {
+    internal class Rekenmachine
+    {
+        public static int TelOp()
+        {
+            
+        }
+    }
     internal class Program
     {
 
     static void Main(string[] args)
     {
+        Monster orge  =new Monster();
+        Monster draak = new Monster();
+        draak.GetHp();
+
+        orge.GetHp();
         Program program = new Program();
-        program Run();
-        // We hebben een variable genaamd "Program" gemaakt, als we Run uitvoeren dan voeren we "Program" uit
+        program.Run();
+
+      
+        Console.WriteLine("Everything works!");
     }
-    
+
+    internal void Run()
+    {
+            
+    }
 
 
+    }
+
+    internal class Monster
+    {
+        int hp = 10;
+
+        public int GetHp()
+        {
+            
+        }
     }
 
 }
