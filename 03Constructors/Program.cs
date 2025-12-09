e40902d6080dad464d1f2de2ea82d42f4fec9ae8
39766MAweb <39766@ma-web.nl>
2025-12-09 12:17:22 +0100

    update

---
+++ 03Constructors/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _03Constructors;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Program program = new Program();
+
+        program.Run();
+    }
+
+    internal void Run()
+    {
+        Console.WriteLine("Helloooo");
+    }
+
+}
---
+++ 03Constructors/QuizVraag.cs
@@ -0,0 +1,13 @@
+﻿namespace _03Constructors;
+
+class QuizVraag
+{
+    internal string vraag;
+    internal string antwoord;
+
+    interal QuizVraag(string vraag, string antwoord)
+    {
+        this.vraag = vraag;
+        this.antwoord = antwoord;
+    }
+}
