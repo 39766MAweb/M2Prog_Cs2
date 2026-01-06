b7b398deea8ee0a007c7ba0ec8ba3ce6d230eeb3
39766MAweb <39766@ma-web.nl>
2026-01-06 11:41:27 +0100

    init

---
+++ 05Dictionaries/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _05Dictionaries;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Program program = new Program();
+
+        program.Run();
+    }
+
+    internal void Run()
+    {
+        Console.WriteLine("05");
+    }   
+
+}
